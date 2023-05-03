53a85891c3ba1c7c5ab00b51842a5cab969c6317
maariyadiminsky <mariya.github@gmail.com>
2023-05-03 11:27:36 -0700

    button text should stay in the middle modal view

--- src/modules/common/components/modal/Modal.tsx
+++ src/modules/common/components/modal/Modal.tsx
@@ -91,7 +91,7 @@ const Modal = (
                   {cancelChildren && (
                     <button
                       type="button"
-                      className="mt-3 inline-flex w-full justify-end rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                       onClick={() => setIsOpen(false)}
                       ref={cancelButtonRef}
                     >
@@ -101,7 +101,7 @@ const Modal = (
                   {confirmChildren && (
                     <button
                       type="button"
-                      className="inline-flex w-full justify-end rounded-md bg-red-600 px-12 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      className="inline-flex w-full justify-center rounded-md bg-red-600 px-12 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                       onClick={handleButtonConfirm}
                     >
                       {confirmChildren}
