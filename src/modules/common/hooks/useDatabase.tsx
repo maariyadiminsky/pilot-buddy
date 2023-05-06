0821a5c50fadb009912c05c6af467f165e54993b
maariyadiminsky <mariya.github@gmail.com>
2023-05-05 22:25:42 -0700

    add more descriptive warning for SESSION_NOT_FOUND

--- src/modules/common/hooks/useDatabase.tsx
+++ src/modules/common/hooks/useDatabase.tsx
@@ -25,7 +25,7 @@ export const DATABASE_ERROR = {
   DATABASE_NOT_FOUND: 'Specified database not found',
   DATA_EXISTS: 'Data already exists in the database',
   TRANSACTION_ABORTED: 'Transaction aborted',
-  SESSION_NOT_FOUND: 'Session Not found',
+  SESSION_NOT_FOUND: "Session Not found. User may have manually routed to another user's session.",
   USER_NOT_FOUND: 'User not found',
   STORAGE_QUOTA: 'Storage quota exceeded',
 };
