76b48e463d0f6a92a436406509c0cf8edf67433a
maariyadiminsky <mariya.github@gmail.com>
2023-05-05 17:30:15 -0700

    image element does not jump when initially loading;

--- src/modules/profile/ProfileCard.tsx
+++ src/modules/profile/ProfileCard.tsx
@@ -13,23 +13,25 @@ interface ProfileCardProps {
 const ProfileCard = ({ wrapperType, user }: ProfileCardProps) => (
   <Menu as="div" className="relative inline-block px-3 text-left">
     <div>
-      <Menu.Button className="group w-full rounded-md bg-gray-100 px-3.5 py-2 text-left text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none border border-gray-200 focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 focus:ring-offset-gray-200">
-        <span className="flex w-full items-center justify-between">
-          <span className="flex min-w-0 items-center justify-between space-x-3">
-            <ProfilePicture wrapperType={wrapperType} src={user?.image} />
-            <span className="flex min-w-0 flex-1 flex-col">
-              <span className="truncate text-sm font-medium text-gray-900">
+      <Menu.Button className="group w-full rounded-md bg-gray-100 px-3 text-left text-sm font-medium text-gray-700 hover:bg-gray-200 focus:outline-none border border-gray-200 focus:ring-2 focus:ring-sky-600 focus:ring-offset-2 focus:ring-offset-gray-200">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex min-w-0 h-16 items-center justify-between space-x-3">
+            <div className="w-12">
+              <ProfilePicture wrapperType={wrapperType} src={user?.image} />
+            </div>
+            <div className="flex min-w-0 flex-1 flex-col">
+              <div className="truncate text-sm font-medium text-gray-900">
                 Welcome{user?.name ? ', ' : ''}
                 {user?.name}!
-              </span>
-              <span className="truncate text-sm text-gray-500">Have a nice day.</span>
-            </span>
-          </span>
+              </div>
+              <div className="truncate text-sm text-gray-500">Have a nice day.</div>
+            </div>
+          </div>
           <ChevronUpDownIcon
             className="h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
             aria-hidden="true"
           />
-        </span>
+        </div>
       </Menu.Button>
     </div>
     <ProfileDropdown {...{ wrapperType }} />
